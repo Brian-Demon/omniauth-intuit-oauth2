7fa15cafd9e43095d841aa066239f9a9f76af130
Demon <BrianWMatejka@gmail.com>
2022-07-04 15:04:37 -0500

    refactor

--- lib/omniauth/strategies/intuit.rb
+++ lib/omniauth/strategies/intuit.rb
@@ -17,10 +17,6 @@ module OmniAuth
 
       option :scope, BASE_SCOPES
 
-      def callback_url
-        full_host + script_name + callback_path
-      end
-
       uid { raw_info['sub'] }
 
       info do
@@ -35,14 +31,13 @@ module OmniAuth
 
       def raw_info
         @raw_info ||= access_token.get(USER_INFO_ENDPOINT).parsed
+        puts "================================================="
+        puts @raw_info
+        puts "================================================="
       end
 
       def verified_email
         raw_info['email_verified'] ? raw_info['email'] : nil
-      end
-
-      def get_token_options(redirect_uri = '')
-        { redirect_uri: redirect_uri }.merge(token_params.to_hash(symbolize_keys: true))
       end
 
       def prune!(hash)
@@ -50,64 +45,6 @@ module OmniAuth
           prune!(value) if value.is_a?(Hash)
           value.nil? || (value.respond_to?(:empty?) && value.empty?)
         end
-      end
-
-      def custom_build_access_token
-        access_token = get_access_token(request)
-
-        verify_hd(access_token)
-        access_token
-      end
-
-      alias build_access_token custom_build_access_token
-
-      def get_access_token(request)
-        verifier = request.params['code']
-        redirect_uri = request.params['redirect_uri']
-        access_token = request.params['access_token']
-        if verifier && request.xhr?
-          client_get_token(verifier, redirect_uri || 'postmessage')
-        elsif verifier
-          client_get_token(verifier, redirect_uri || callback_url)
-        elsif access_token && verify_token(access_token)
-          ::OAuth2::AccessToken.from_hash(client, request.params.dup)
-        elsif request.content_type =~ /json/i
-          begin
-            body = JSON.parse(request.body.read)
-            request.body.rewind # rewind request body for downstream middlewares
-            verifier = body && body['code']
-            access_token = body && body['access_token']
-            redirect_uri ||= body && body['redirect_uri']
-            if verifier
-              client_get_token(verifier, redirect_uri || 'postmessage')
-            elsif verify_token(access_token)
-              ::OAuth2::AccessToken.from_hash(client, body.dup)
-            end
-          rescue JSON::ParserError => e
-            warn "[omniauth google-oauth2] JSON parse error=#{e}"
-          end
-        end
-      end
-
-      def client_get_token(verifier, redirect_uri)
-        client.auth_code.get_token(verifier, get_token_options(redirect_uri), get_token_params)
-      end
-
-      def get_token_params
-        deep_symbolize(options.auth_token_params || {})
-      end
-
-      def verify_hd(access_token)
-        return true unless options.hd
-
-        @raw_info ||= access_token.get(USER_INFO_ENDPOINT).parsed
-
-        options.hd = options.hd.call if options.hd.is_a? Proc
-        allowed_hosted_domains = Array(options.hd)
-
-        raise CallbackError.new(:invalid_hd, 'Invalid Hosted Domain') unless allowed_hosted_domains.include?(@raw_info['hd']) || options.hd == '*'
-
-        true
       end
     end
   end
